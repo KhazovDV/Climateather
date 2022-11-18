0ec4af1a14af81a0927d81c0f49024a9e02ab9a7
KhazovDV <101877272+KhazovDV@users.noreply.github.com>
2022-11-18 15:33:59 +0300

    added cleaning textField

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -17,18 +17,32 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var searchTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+
      
         searchTextField.delegate = self
     }
 
     @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
         print(searchTextField.text!)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
         print(searchTextField.text!)
         return true
     }
     
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            searchTextField.text = ""
+        }
+    }
 }
 
